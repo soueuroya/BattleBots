b777a7ecd6bbfa86906075d7682f53cba3058916
DanielCBoldrin <daniel_cboldrin@hotmail.com>
2022-06-11 19:03:36 -0400

    Started Celldragging

--- Assets/Scripts/GridControlScripts/CellScript.cs
+++ Assets/Scripts/GridControlScripts/CellScript.cs
@@ -3,5 +3,32 @@ using static StaticHelper;
 
 public class CellScript : MonoBehaviour
 {
+    public Transform tr;
     public PieceType type;
+    public bool hovering = false;
+    public Vector3 originalScale;
+
+    void OnValidate()
+    {
+        tr = transform;
+        originalScale = tr.localScale;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!hovering)
+        {
+            hovering = true;
+            tr.localScale *= 1.2f;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (hovering)
+        {
+            hovering = false;
+            tr.localScale = originalScale;
+        }
+    }
 }
---
+++ Assets/SocketCellScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocketCellScript : CellScript
+{
+    public bool isSocketed = false;
+    public CellDragScript socketedCell;
+    public Vector2 XY;
+    
+
+
+
+}
