3e673d217dac622da8a5b44bb6bea3850118c496
Borzigenti <1heted@hdsb.ca>
2022-06-24 19:38:47 -0400

    Made Center Cell Locked

--- Assets/Scripts/GridControlScripts/GridScript.cs
+++ Assets/Scripts/GridControlScripts/GridScript.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class GridScript : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    public CellScript cellPrefab;
+    public CellSelectScript currentCell;
+    public CellScript[,] grid = new CellScript[GRIDSIZE, GRIDSIZE]; // 9x9 = 81
+    public Transform startingPos;
+    public static GridScript Instance;
+    public Material coreMat;
+>>>>>>> Stashed changes
 
     public CellScript cells;
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         
+=======
+        if (cellPrefab != null)
+        {
+            for (int i = 0; i < GRIDSIZE; i++)
+            {
+                for (int j = 0; j < GRIDSIZE; j++)
+                {
+                    grid[i, j] = Instantiate(cellPrefab, transform);
+                    if (i == Mathf.Round(GRIDSIZE/2) && j == Mathf.Round(GRIDSIZE/2))
+                    {
+                        grid[i, j].locked = true;
+                        grid[i, j].type = PieceType.CORE;
+                        grid[i, j].meshRenderer.material = coreMat;
+                    }
+                    grid[i, j].transform.position = startingPos.position + transform.right * i * 0.065f + transform.forward * j * -0.065f;
+
+                    grid[i, j].transform.Translate(transform.up * 0.001f);
+                    //grid[i, j].transform.Translate(transform.forward * j * 0.07f);
+
+                }
+            }
+        }
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
